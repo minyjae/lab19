f76434493c44e4b9c1b49031d73535fadd88adf9
minyjae <120163272+minyjae@users.noreply.github.com>
2023-02-21 21:04:47 +0700

    Update lab19_1.cpp
    complete lab19

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -6,34 +6,92 @@
 
 using namespace std;
 
-char score2grade(int score){
-    if(score >= 80) return 'A';
-    if(score >= 70) return 'B';
-    if(score >= 60) return 'C';
-    if(score >= 50) return 'D';
-    else return 'F';
+char score2grade(int score)
+{
+    if (score >= 80)
+        return 'A';
+    if (score >= 70)
+        return 'B';
+    if (score >= 60)
+        return 'C';
+    if (score >= 50)
+        return 'D';
+    else
+        return 'F';
 }
 
-string toUpperStr(string x){
+string toUpperStr(string x)
+{
     string y = x;
-    for(unsigned i = 0; i < x.size();i++) y[i] = toupper(x[i]);
+    for (unsigned int i = 0; i < x.size(); i++)
+        y[i] = toupper(x[i]);
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string f, vector<string> &n, vector<int> &s, vector<char> &g)
+{
+    ifstream read(f);
+    string txt = "";
+    char format[] = "%[^:]: %d %d %d";
+    while (getline(read, txt))
+    {
+        char name[100];
+        int sum = 0, s1 = 0, s2 = 0, s3 = 0;
+        sscanf(txt.c_str(), format, name, &s1, &s2, &s3);
+        sum = s1 + s2 + s3;
+        n.push_back(name);
+        s.push_back(sum);
+    }
+    for (unsigned int i = 0; i < n.size(); i++)
+        g.push_back(score2grade(s[i]));
 }
 
-void getCommand(){
-
+void getCommand(string &cmd, string &key)
+{
+    string line = "";
+    cout << "Please input your command: ";
+    getline(cin,line);
+    int start = 0;
+    int end = line.find_first_of(" ");
+    cmd = line.substr(start,end);   
+    key = line.substr(end+1,line.length());
 }
 
-void searchName(){
-
+void searchName(vector<string> &n, vector<int> &s, vector<char> &g, string key)
+{
+    bool found = false;
+    cout << "---------------------------------\n";
+    for (unsigned int i = 0; i < n.size(); i++)
+    {
+        if (toUpperStr(n[i]) == key)
+        {
+            cout << n[i] << "'s score = " << s[i];
+            cout << "\n";
+            cout << n[i] << "'s grade = " << g[i];
+            cout << "\n";
+            found = true;
+        }
+        else if (i == n.size() - 1 && !found)
+            cout << "Cannot found.\n";
+    }
+    cout << "---------------------------------\n";
 }
+void searchGrade(vector<string> &n, vector<int> &s, vector<char> &g, string key)
 
-void searchGrade(){
-
+{
+    bool found = false;
+    cout << "---------------------------------\n";
+    for (unsigned int i = 0; i < n.size(); i++)
+    {
+        if (g[i] == key[0])
+        {
+            cout << n[i] << " (" << s[i] << ")\n";
+            found = true;
+        }
+        else if (i == n.size() - 1 && !found)
+            cout << "Cannot found.\n";
+    }
+    cout << "---------------------------------\n";
 }
 
 
